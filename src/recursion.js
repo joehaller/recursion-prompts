602445cf6b9c768171ec736c3804337661ad201c
Joseph Haller <j.edward.haller@gmail.com>
2020-10-27 10:29:19 -0400

    Implemented sumBelow

--- src/recursion.js
+++ src/recursion.js
@@ -53,6 +53,15 @@ var isEven = function(n) {
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+  if (n <= 1 && n > -1) {
+    return 0;
+  } else if (n > 0) {
+    n = n - 1;
+    return n + sumBelow(n);
+  } else {
+    n = n + 1;
+    return n + sumBelow(n);
+  }
 };
 
 // 6. Get the integers within a range (x, y).

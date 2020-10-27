f0892a2a037b5f6a7babcacc42a3f4b101dadcbf
Joseph Haller <j.edward.haller@gmail.com>
2020-10-27 13:14:41 -0400

    Implemented palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -122,6 +122,14 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  string = string.toLowerCase();
+  if (string.length === 0 || string.length === 1) {
+    return true;
+  } else if (string[0] !== string[string.length -1]) {
+    return false;
+  } else {
+    return palindrome(string.substring(1, string.length - 1));
+  }
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the

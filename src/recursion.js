e6625b827720f30d45f6575001b2ad3a84e3db4e
Joseph Haller <j.edward.haller@gmail.com>
2020-10-27 12:58:32 -0400

    Implemented reverse

--- src/recursion.js
+++ src/recursion.js
@@ -113,6 +113,11 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  if (string.length === 0) {
+    return "";
+  } else {
+    return reverse(string.substring(1)) + string.slice(0, 1);
+  }
 };
 
 // 10. Write a function that determines if a string is a palindrome.
